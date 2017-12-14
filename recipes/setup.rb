63b64387adc9bbbf822aae9e5116718cd0cae477
vagrant <vagrant@localhost.localdomain>
2017-12-14 16:01:26 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,27 @@
+package 'vim-enhanced'
+
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+service 'ntpd' do
+  action [:enable, :start]
+end
+
+file '/etc/motd' do
+  content 'This computer is the property of Richard...'
+  action :create
+  mode  '0655'
+  owner 'root'
+  group 'root'
+end
+
+
